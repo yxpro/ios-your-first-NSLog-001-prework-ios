42057372bce55dba269f0d5007e9f140a595c513
yxpro <yxpro@users.noreply.github.com>
2016-05-10 19:17:57 -0400

    NSLog in XCode

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -6,6 +6,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    NSLog(@"Hello Cruel World!");
+    
     /**
     
      * Hey! Write your code here! :D
